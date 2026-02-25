83cba67afb027ed4afe7d895de0237d7e793bb04
Darwin lopez <guelopezadrian@gmail.com>
2026-02-25 17:13:15 -0500

    Corregir URL del backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: process.env.NODE_ENV === 'production' 
+        ? 'https://parkcol.onrender.com/api' 
+        : 'http://localhost:5000/api'
 });
 
-// Interceptor para agregar token a las peticiones
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
     if (token) {
